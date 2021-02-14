6d3b60ca9e6ee769fb970c775f6954668bc4d4ca
Philippe Loctaux <p@philippeloctaux.com>
2021-02-14 19:09:56 +0100

    Initial Commit

---
+++ pass-menubar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  pass-menubar
+//
+//  Created by phil on 14/02/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ pass-menubar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  pass-menubar
+//
+//  Created by phil on 14/02/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
