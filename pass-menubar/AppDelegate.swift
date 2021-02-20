3385349dd85e7b998be7211582c9694d373480f2
Philippe Loctaux <p@philippeloctaux.com>
2021-02-20 16:10:21 +0100

    added menubar app, removed dock icon, added menu and app icon

--- pass-menubar/AppDelegate.swift
+++ pass-menubar/AppDelegate.swift
@@ -9,9 +9,24 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     
-
+    var statusItem: NSStatusItem?
+    @IBOutlet weak var menu: NSMenu?
+    @IBOutlet weak var firstMenuITem: NSMenuItem?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+     
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        let itemImage = NSImage(named: "lock.fill")
+        itemImage?.isTemplate = true
+        statusItem?.button?.image = itemImage
+        
+        if let menu = menu {
+            statusItem?.menu = menu
+        }
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
