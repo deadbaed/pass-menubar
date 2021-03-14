ae3b07fd3e31a2f85c38d7f78f7ed916c822e31a
Philippe Loctaux <p@philippeloctaux.com>
2021-03-15 00:04:04 +0100

    basic class for easy handling of paths

--- pass-menubar/GeneralPreferenceViewController.swift
+++ pass-menubar/GeneralPreferenceViewController.swift
@@ -20,23 +20,64 @@ let GeneralPreferencesViewController: () -> PreferencePane = {
     return Preferences.PaneHostingController(pane: paneView)
 }
 
+private class Path {
+    private enum Kind {
+        case Unknown
+        case File
+        case Directory
+    }
+
+    private var path: String
+    private var kind: Kind
+
+    init() {
+        path = ""
+        kind = Kind.Unknown
+    }
+
+    func load(newPath: String) {
+        if newPath.isEmpty {
+            return
+        }
+        path = newPath
+        // set kind here
+        print(path)
+    }
+
+    func isDir() -> Bool {
+        kind == Kind.Directory ? true : false
+    }
+
+    func isFile() -> Bool {
+        kind == Kind.File ? true : false
+    }
+}
+
 struct GeneralView: View {
     @AppStorage("rememberPassphrase") private var rememberPassphrase = false
     @AppStorage("useTouchID") private var useTouchID = false
+    @AppStorage("rawPathPass") private var rawPathPass = ""
+    @AppStorage("rawPathKey") private var rawPathKey = ""
+    private var pass = Path()
+    private var key = Path()
     private let contentWidth: Double = 450.0
-
-    func test() {}
 
     var body: some View {
         Preferences.Container(contentWidth: contentWidth) {
             Preferences.Section(title: "Password store location:") {
-                Button("Choose directory...", action: test)
-                Text("Location of password store to use.")
+                HStack {
+                    TextField("Enter path of directory", text: $rawPathPass)
+                    Button("Validate", action: { pass.load(newPath: rawPathPass) })
+                }.onAppear { pass.load(newPath: rawPathPass) }
+                Text("Say here if password store has been detected or not")
                         .preferenceDescription()
             }
             Preferences.Section(title: "PGP key location:") {
-                Button("Choose file...", action: test)
-                Text("Location of key to use when decrypting passwords.")
+                HStack {
+                    TextField("Enter path of file", text: $rawPathKey)
+                    Button("Validate", action: { key.load(newPath: rawPathKey) })
+                }.onAppear { key.load(newPath: rawPathKey) }
+                Text("Say here if key has been found or not")
                         .preferenceDescription()
             }
             Preferences.Section(title: "Security") {
