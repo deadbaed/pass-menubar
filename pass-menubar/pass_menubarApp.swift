f34e436d452e82f5822cd39c84d36bf6de892355
Philippe Loctaux <p@philippeloctaux.com>
2021-03-25 13:52:09 +0100

    added new swiftui life cycle project

---
+++ pass-menubar/pass_menubarApp.swift
@@ -0,0 +1,17 @@
+//
+//  pass_menubarApp.swift
+//  pass-menubar
+//
+//  Created by phil on 25/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct pass_menubarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
