edb930c2f2471c44ff17cb680a6374168dde268c
Philippe Loctaux <p@philippeloctaux.com>
2021-03-30 14:58:34 +0200

    search bar in the top

--- pass-menubar/ContentView.swift
+++ pass-menubar/ContentView.swift
@@ -14,16 +14,19 @@ func get_list_passwords() -> [String] {
 }
 
 struct ContentView: View {
-    @Binding var search: String
-    @AppStorage("rawPathPass") private var rawPathPass = ""
+    @State var search = ""
     
     let list_files = get_list_passwords()
 
     var body: some View {
         VStack {
             HStack {
-                Text(rawPathPass)
-                Spacer()
+                // search
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                    TextField("Search", text: $search)
+                }
+
                 // settings
                 Button(action: { NSApp.sendAction(Selector(("showPreferencesWindow:")), to: nil, from: nil) },
                        label: {
@@ -36,33 +39,22 @@ struct ContentView: View {
                 }, label: {
                     Image(systemName: "power")
                 })
-            }
-
-           /* HStack {
-                Image(systemName: "magnifyingglass")//.padding(.leading, 5)
-
-                TextField("", text: $searchPassword).textFieldStyle(PlainTextFieldStyle())
-                    .padding(0).font(.subheadline)
-
-                Button(action: { self.searchPassword = "" }, label: { Image(systemName: "xmark.circle.fill") }).disabled(searchPassword.isEmpty)
-                .buttonStyle(BorderlessButtonStyle())
-            }.padding(5) */
+            }.padding(5)
 
             ScrollView {
                 VStack(alignment: .leading) {
                     ForEach(0 ..< list_files.count) { value in
-                        Text(list_files[value]).padding(EdgeInsets(top: 1, leading: 0, bottom: 1, trailing: 0))
-                        /* Button(action: { print(list_files[value])}, label: { Text(list_files[value]).padding(1) }) */
+                        Text(list_files[value]).padding(.leading, 5)
+                        Divider()
                     }
                 }.frame(maxWidth: .infinity, alignment: .topLeading)
             }
         }.padding(5)
-//        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(search: .constant(""))
+        ContentView()
     }
 }
