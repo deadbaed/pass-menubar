0efadc0ef868c42df541b689e32b169f2c70879e
Philippe Loctaux <p@philippeloctaux.com>
2021-04-06 22:15:23 +0200

    code cleanup: use File.path(relativeTo: Folder)

--- pass-menubar/Password.swift
+++ pass-menubar/Password.swift
@@ -8,20 +8,6 @@
 import Foundation
 import Files
 
-/// Get parent folders of file
-private func get_parent_folders(root_folder: Folder, file: File) -> String {
-    var folder : Folder = file.parent!
-    var string = ""
-
-    // While we don't reach root folder, add each parent folder to array
-    while folder != root_folder {
-        string = folder.name + "/" + string
-        folder = folder.parent!
-    }
-
-    return string
-}
-
 struct Password: Identifiable {
     let id = UUID()
     var file: File
@@ -31,12 +17,7 @@ struct Password: Identifiable {
         self.file = file
 
         // Construct display name
-        self.display = self.file.name
+        self.display = self.file.path(relativeTo: rootFolder)
         self.display.removeLast(4) // Don't display ".gpg"
-
-        // Display subfolder names inside password store
-        let displayFolders = get_parent_folders(root_folder: rootFolder, file: self.file)
-
-        self.display = displayFolders + self.display
     }
 }
