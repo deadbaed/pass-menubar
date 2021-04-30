5cdfb407e7c6b79de7dc16e12cf9e3c1ce4ee977
Philippe Loctaux <p@philippeloctaux.com>
2021-04-30 21:06:06 +0200

    added timer when decrypting password, close window when timer expires

--- pass-menubar/DecryptView.swift
+++ pass-menubar/DecryptView.swift
@@ -14,6 +14,10 @@ func copyClipboard(str: String) -> Bool {
     return pasteboard.setString(str, forType: .string)
 }
 
+func clearClipboard() -> Bool {
+    return copyClipboard(str: "")
+}
+
 func getUserId(path: String) -> String {
     var user = ""
 
@@ -33,50 +37,67 @@ struct DecryptView: View {
     @AppStorage("rawPathKey") private var rawPathKey = ""
     @State private var passphrase = ""
     @State private var invalidPassphrase = false
+    @State private var askPassphrase = true
+    @State private var timeLeft = 45
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
-        VStack() {
-            Text("Passphrase").font(.title)
-            Text("Enter your passphrase to unlock the secret key.")
-            Spacer()
-            HStack {
-                Text("Key ID: ")
-                Text(getUserId(path: rawPathKey))
+        if (askPassphrase) {
+            VStack() {
+                Text("Passphrase").font(.title)
+                Text("Enter your passphrase to unlock the secret key.")
                 Spacer()
-            }
-            HStack {
-                Text("Passphrase: ")
-                SecureField("", text: $passphrase)
-                Spacer()
-            }
-            if invalidPassphrase {
-                Text("You entered an invalid passphrase. Please try again.").fontWeight(.bold)
-            }
-        }.padding()
-        Spacer()
-        VStack(alignment: .trailing) {
-            HStack {
-                Spacer()
-                Button("Cancel", action: {
-                    NSApplication.shared.keyWindow?.close()
-                }).keyboardShortcut(.cancelAction)
-                Button("Decrypt", action: {
-                    do {
-                        let result = try decrypt(path: password.path, key: rawPathKey, passphrase: passphrase, line: 0)
-                        print("decrypted passpword: \(result)")
-                        invalidPassphrase = false
-                        if copyClipboard(str: result) == true {
-                            print("copied to clipboard")
-                            // todo: replace contents of window with another view
-                        } else {
-                            print("failed to copy to clipboard")
-                        }
-                        // TODO: handle error cases and update message
-                    } catch {
-                        invalidPassphrase = true
-                    }
-                }).keyboardShortcut(.defaultAction)
+                HStack {
+                    Text("Key ID: ")
+                    Text(getUserId(path: rawPathKey))
+                    Spacer()
+                }
+                HStack {
+                    Text("Passphrase: ")
+                    SecureField("", text: $passphrase)
+                    Spacer()
+                }
+                if invalidPassphrase {
+                    Text("You entered an invalid passphrase. Please try again.").fontWeight(.bold)
+                }
             }.padding()
+            Spacer()
+            VStack(alignment: .trailing) {
+                HStack {
+                    Spacer()
+                    Button("Cancel", action: {
+                        NSApplication.shared.keyWindow?.close()
+                    }).keyboardShortcut(.cancelAction)
+                    Button("Decrypt", action: {
+                        do {
+                            let result = try decrypt(path: password.path, key: rawPathKey, passphrase: passphrase, line: 0)
+                            print("decrypted passpword: \(result)")
+                            invalidPassphrase = false
+                            if copyClipboard(str: result) == true {
+                                print("copied to clipboard")
+                                askPassphrase = false
+                            } else {
+                                print("failed to copy to clipboard")
+                            }
+                            // TODO: handle error cases and update message
+                        } catch {
+                            invalidPassphrase = true
+                        }
+                    }).keyboardShortcut(.defaultAction)
+                }.padding()
+            }
+        } else {
+            Text("Password has been copied to clipboard.")
+            Text("Will clear in \(timeLeft) seconds.")
+                .onReceive(timer) { _ in
+                    if timeLeft > 0 {
+                        timeLeft -= 1
+                    } else {
+                        // Clear clipboard and close window when timer is over
+                        clearClipboard()
+                        NSApplication.shared.keyWindow?.close()
+                    }
+                }
         }
     }
 }
