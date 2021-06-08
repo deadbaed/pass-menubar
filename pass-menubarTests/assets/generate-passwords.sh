a9d151af321f40d459366e3d9900886d7526afd3
Philippe Loctaux <p@philippeloctaux.com>
2021-06-08 08:22:17 +0200

    generate-passwords: regex to keep alpha strings

--- pass-menubarTests/assets/generate-passwords.sh
+++ pass-menubarTests/assets/generate-passwords.sh
@@ -2,7 +2,11 @@
 #
 
 # get 512 random words from english dictionary and store in array
-passwords=( $(awk 'BEGIN {srand()} {print rand() " " $0}' /usr/share/dict/usa | sort | head -512 | awk '{print $2}'))
+
+passwords=( $(awk 'BEGIN {srand()} {print rand() " " $0}' /usr/share/dict/usa | sort | head -1024 | awk '{print $2}' | grep -E '^[a-zA-Z0-9_]+$'))
 
 # display each entry of array
 for (( i=0; i<${#passwords[@]}; i++ )); do echo ${passwords[i]}; done
+
+# to add new passwords
+# echo "password_name" | pass insert -m password_name
